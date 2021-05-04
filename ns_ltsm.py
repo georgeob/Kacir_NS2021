e1236ec576a166a3ab7253337e5b4ff44c5bc16b
Michal <michal.kacir@gmail.com>
2021-05-04 21:24:48 +0200

    Normalizacia dat a preprocessing

--- ns_ltsm.py
+++ ns_ltsm.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 import numpy as np
+import random
+from sklearn import preprocessing
+from collections import deque
 
 SEQ_LEN = 60 # dlzka sekvencie
 FUTURE_PERIOD_PREDICT = 3 # kolko minut dopredu chceme predikovat rast/klesanie
@@ -12,6 +15,59 @@ def classify(current, future):
         return 1
     else:
         return 0
+
+# Preprocesing a sekvencie
+def preprocess_df(df):
+    df = df.drop( f"{PAIR_TO_PREDICT}_future", 1 ) # odstranenie future stlpca - sluzil len pre vytvorenie target stlpca
+
+    for col in df.columns:
+        if col != f"{PAIR_TO_PREDICT}_target":
+            df[col] = df[col].pct_change() # normalizacia hodnot na percenta
+            df.dropna(inplace=True) # remove the nas created by pct_change
+            df[col] = preprocessing.scale(df[col].values) # skalovanie medzi 0 a 1
+    
+    df.dropna(inplace=True) # pri normalizacii mohly vzniknut prazdne hodnoty
+
+    sequential_data = []  # list pre ukladanie sekvencnych dat
+    prev_days = deque(maxlen=SEQ_LEN)  # sekvencie
+
+    # print(df.values)
+
+    for i in df.values:  # iterovanie riadkami
+        prev_days.append([n for n in i[:-1]])  # store all but the target
+        if len(prev_days) == SEQ_LEN:  # make sure we have 60 sequences!
+            sequential_data.append([np.array(prev_days), i[-1]])  # append feature and labels sequences
+
+    random.shuffle(sequential_data)  # pomiesanie dat
+
+    buys = []  # sekvencia nakupovacich - buy hodnot
+    sells = []  # sekvencia predavacich - sel hodnot
+
+    for seq, target in sequential_data:  # iterovanie sekvencnymi datami
+        if target == 0: 
+            sells.append([seq, target])
+        elif target == 1:
+            buys.append([seq, target]) 
+
+    random.shuffle(buys)
+    random.shuffle(sells)
+
+    # balancovanie buy/sell hodnot
+    lower = min(len(buys), len(sells)) 
+    buys = buys[:lower]
+    sells = sells[:lower]
+
+    sequential_data = buys+sells  # spojenie do sekvencie
+    random.shuffle(sequential_data)
+
+    X = []
+    y = []
+
+    for seq, target in sequential_data:
+        X.append(seq)  # sekvencia
+        y.append(target)  # nazvy - buy/sell
+
+    return np.array(X), y
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)), )
 
@@ -46,3 +102,24 @@ master_df.fillna(method="ffill", inplace=True)  # ak existuju medzery v udajoch,
 master_df.dropna(inplace=True) # odstranenie na hodnot
 
 # print( master_df.head() )
+
+master_df[f"{PAIR_TO_PREDICT}_future"] = master_df[f"{PAIR_TO_PREDICT}_close"].shift(-FUTURE_PERIOD_PREDICT) # vytorenie future stlpca na zaklade velkosti predikcie
+
+master_df[f"{PAIR_TO_PREDICT}_target"] = list(map(classify, master_df[f"{PAIR_TO_PREDICT}_close"], master_df[f"{PAIR_TO_PREDICT}_future"])) # vytvorenie target stlpca
+
+# print( master_df[[f"{PAIR_TO_PREDICT}_close", f"{PAIR_TO_PREDICT}_future", f"{PAIR_TO_PREDICT}_target"]].head(10) )
+
+times = sorted(master_df.index.values)
+last_5pct = sorted(master_df.index.values)[-int(0.05*len(times))]  # poslednych 5% vsetkych dat na verifikaciu
+
+# print( last_5pct )
+
+validation_master_df = master_df[(master_df.index >= last_5pct)]  # validacne data budu poslednych 5%
+master_df = master_df[(master_df.index < last_5pct)]  # odstranenie poslednych 5% ktore su vo validacii
+
+train_x, train_y = preprocess_df(master_df)
+validation_x, validation_y = preprocess_df(validation_master_df)
+
+print(f"Trenovacie data: {len(train_x)} Validacne data: {len(validation_x)}")
+print(f"Predaje: {train_y.count(0)}, Nakupy: {train_y.count(1)}")
+print(f"Validacia predaje: {validation_y.count(0)}, Validacia nakupy: {validation_y.count(1)}")
